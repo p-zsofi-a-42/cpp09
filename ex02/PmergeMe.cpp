aa86b29738a5f713fc46c6ccc0549260c22d71b9
p-zsofi-a-@-42 <177267292+p-zsofi-a-42@users.noreply.github.com>
2026-03-31 15:03:11 +0200

    wip: ex02 pseudo code for part 1

--- ex02/PmergeMe.cpp
+++ ex02/PmergeMe.cpp
@@ -6,7 +6,7 @@
 /*   By: zpalotas <zpalotas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/26 15:08:57 by zpalotas          #+#    #+#             */
-/*   Updated: 2026/03/30 17:50:20 by zpalotas         ###   ########.fr       */
+/*   Updated: 2026/03/31 15:02:32 by zpalotas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,9 +24,9 @@ PmergeMe::PmergeMe(std::stringstream &input)
 	{
 		input >> element;
 		if (!input.fail())
-			start_sequence.push_back(element);
+			input_sequence.push_back(element);
 	}
-	std::for_each(start_sequence.begin(), start_sequence.end(), myPrint);
+	std::for_each(input_sequence.begin(), input_sequence.end(), myPrint);
 	std::cout << std::endl;
 }
 
@@ -46,18 +46,78 @@ PmergeMe &PmergeMe::operator=(const PmergeMe &other)
 	return (*this);
 }
 
-void PmergeMe::FormPairs()
+void PmergeMe::FormPairs() // only on first lvl
 {
-	std::list<int>::iterator it;
-	std::list<int>::iterator next;
+	while (input_sequence.size() >= 2)
+	{
+		make pair(list.begin, list next)
+	}
+
+//	std::for_each(result_sequence.begin(), result_sequence.end(), myPrintPair);
+//	std::cout << "\n" << comparison_counter_ << std::endl;
+}
+
+void PmergeMe::compare()
+{
+	while (start seq)
+	{
+		if (first.back() > second.back())
+			flip
+		comparison_counter_++;
+	}	
+}
+
+void PmergeMe::resultList()
+{
+	while(full list.size >= 2)
+		pair,first.merge(second)
+	while(full list)
+		(if (! next || next.size != 2^recursionlvl))
+			reserve.push(next);
+		else
+			pair.second = next
+		pop(next)
+	//now we have a list of  pairs with doube the size
+}
+
+void PmergeMe::sort()
+{
+	FormPairs();
+	compare()
+	resultList();
+	if (result_sequence.size > 2)
+		sort();
+	//part2
+
+}
+
+/* void PmergeMe::FormPairs()
+{
+	std::list<std::pair<std::list<int>, std::list<int>> >::iterator it;
+	std::list<std::pair<std::list<int>, std::list<int>> >::iterator next;
 	while (start_sequence.size() >= 2)
 	{
-		it = start_sequence.begin();
-		next = it;
-		next++;
-		if (next == start_sequence.end())
-			break;;
-		std::pair<int, int> myPair = std::make_pair(*it, *next);
+		if (recursion_lvl_ = 0)
+		{
+			//
+		}
+		else
+		{
+			std::pair<std::list<int>, std::list<int> > myPair = std::make_pair(*it, *next);
+
+			it = start_sequence.begin();
+			next = it;
+			next++;
+			if (next == start_sequence.end())
+				break;;
+		}
+		
+		
+		else
+		{
+			std::list<int> = 
+			std::pair< std::list<int>> myPair = std::make_pair(*it, *next);
+		}
 		if (myPair.first > myPair.second)
 		{
 			int temp = myPair.first;
@@ -75,7 +135,7 @@ void PmergeMe::FormPairs()
 	std::for_each(result_sequence.begin(), result_sequence.end(), myPrintPair);
 
 	std::cout << "\n" << comparison_counter_ << std::endl;
-}
+} */
 
 void	myPrint(int value)
 {
--- ex02/PmergeMe.hpp
+++ ex02/PmergeMe.hpp
@@ -6,7 +6,7 @@
 /*   By: zpalotas <zpalotas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/26 15:09:00 by zpalotas          #+#    #+#             */
-/*   Updated: 2026/03/30 17:50:20 by zpalotas         ###   ########.fr       */
+/*   Updated: 2026/03/31 15:02:57 by zpalotas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,22 +28,28 @@ class PmergeMe
 	private:
 		PmergeMe();
 
-		std::list<int> start_sequence;
-		std::list<int> higher;
-		std::list<int> lower;
-		std::list<std::pair<int, int>  > result_sequence;
+		std::list<int> input_sequence;
+		std::list<std::pair<std::list<int>, std::list<int>>  > start_sequence;
+		std::list<int> main;
+		std::list<int> pend;
+		std::list<std::pair<std::list<int>, std::list<int>>  > result_sequence;
 		std::list<int> reserve;
 
 		int	comparison_counter_; //counts comparisons
 		int	recursion_lvl_; //pair size = 2^recusion_lvl, how many elements belong to a pair at start 1-1 then 2-2
 	
+		void FormPairs(); //TODO priv
+		void compare(); //TODO priv
+		void resultList(); //TODO priv
+
 	public:
 		PmergeMe(std::stringstream &input);
 		~PmergeMe();
 		PmergeMe(const PmergeMe &other);
 
 		PmergeMe &operator= (const PmergeMe &other);
-		void FormPairs();
+
+		void sort();
 };
 	
 void	myPrint(int value);
