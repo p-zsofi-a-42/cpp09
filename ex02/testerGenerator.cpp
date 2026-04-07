2b84fdc7edea5180ae77bd1747a0bf73a9f40e0d
p-zsofi-a-@-42 <177267292+p-zsofi-a-42@users.noreply.github.com>
2026-04-07 21:19:30 +0200

    chore: ex02 added tester, sometimes too many comparisons, and accidental insertion of -1

---
+++ ex02/testerGenerator.cpp
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   testerGenerator.cpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zpalotas <zpalotas@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/07 21:18:46 by zpalotas          #+#    #+#             */
+/*   Updated: 2026/04/07 21:18:47 by zpalotas         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <cmath>
+#include <climits>
+#include <cstdlib>
+#include <fstream>		// std::ofstream
+
+int main(int argc, char **argv)
+{
+	if (argc != 2)
+	{
+		std::cerr << "Please provide only one argument (n)." << std::endl;
+		return (0);
+	}
+	std::ofstream	file_to_write("test");//	file_to_read.open(filename);
+	if(!file_to_write.is_open())
+		{	std::cerr 	<< "Error: [ output file ] could not be opened" << std::endl; return 1; }
+	
+	int n = atoi(argv[1]);
+	//printing the limit
+	unsigned int sum = 0;
+	for (int k = 1; k <= n; k++)
+	{
+		sum += ceil(log2((3.0/4.0)*k));
+	}
+	std::cout << "For [" << n << "] number of elements the maximum acceptable comparisons are: "
+			<< "\033[1;32m" << sum << std::endl;
+
+	//generating the numbers
+	srand(time(NULL));
+	int num;
+	while (n--)
+	{
+		num = std::rand(); //NOTE wo't go to int may, so limits should be checked manually
+		while (num > 100)
+			num /= 10;
+		file_to_write << num;
+		file_to_write << " ";
+	}
+	file_to_write.close();
+	return 0;
+}
