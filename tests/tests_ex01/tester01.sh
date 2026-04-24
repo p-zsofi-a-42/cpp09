e14391ad3594f51fce3766a3432a8321306fa41d
p-zsofi-a-@-42 <177267292+p-zsofi-a-42@users.noreply.github.com>
2026-04-24 14:34:52 +0200

    refactor: ex01 explicit error is printed on cerr

--- tests/tests_ex01/tester01.sh
+++ tests/tests_ex01/tester01.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
+short_output=$(echo "$output" | tail -n 1)
 echo -e "--------------------------------------------------------------------"
-if [ "$output" == "$expected_output" ]; then
+if [ "$short_output" == "$expected_output" ]; then
 	echo -e -n "${YELLOW_B}"	"\b$name ✅\n" "${RESET}"
 	((pass_counter++))
 	echo -e	"${WHITE_B}"	"\b\tInput:\n"		"\b${WHITE}"		"\b$args"	"\b${RESET}"
-	echo -e	"${WHITE_B}"	"\b\tGot:\n"		"\b${WHITE}"		"\b$expected_output"	"\b${RESET}"
+	echo -e	"${WHITE_B}"	"\b\tGot:\n"		"\b${WHITE}"		"\b$output"	"\b${RESET}"
 else
 	((fail_counter++))
 	echo -e "${YELLOW_B}"	"\b$name ❌"
