747e0a3445cb858cc5567e8e0daf7024cc24c010
p-zsofi-a-@-42 <177267292+p-zsofi-a-42@users.noreply.github.com>
2026-04-24 18:52:44 +0200

    refactor: ex01 enforcing vector for stack

--- ex01/RPN.hpp
+++ ex01/RPN.hpp
@@ -6,7 +6,7 @@
 /*   By: zpalotas <zpalotas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/13 17:32:45 by zpalotas          #+#    #+#             */
-/*   Updated: 2026/03/20 14:30:32 by zpalotas         ###   ########.fr       */
+/*   Updated: 2026/04/24 18:52:12 by zpalotas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@
 # include <string>
 # include <sstream>		//stringstream
 # include <stack>
+# include <vector>
 # include <algorithm>
 # include <climits>
 # include "colors.hpp"
@@ -25,7 +26,7 @@ class RPN
 {
 	private:
 		RPN();
-		std::stack<double> expr;
+		std::stack<double, std::vector<double> > expr;
 
 		void addition();
 		void substraction();
