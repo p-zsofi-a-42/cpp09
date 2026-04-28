4ef7c04979e2f7db479aaceeb73e59591d85491f
p-zsofi-a-@-42 <177267292+p-zsofi-a-42@users.noreply.github.com>
2026-04-28 14:25:16 +0200

    wip: ex02 temp fix for printer funcs

--- ex02/printer.cpp
+++ ex02/printer.cpp
@@ -6,7 +6,7 @@
 /*   By: zpalotas <zpalotas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/16 18:03:24 by zpalotas          #+#    #+#             */
-/*   Updated: 2026/04/16 18:09:15 by zpalotas         ###   ########.fr       */
+/*   Updated: 2026/04/28 14:24:16 by zpalotas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,7 +32,7 @@ void	myPrintListList(std::list< std::list<int> > listList, std::string id)
 	}
 }
 
-void	myPrintPair(pendMain value)
+void	myPrintPair(pendMain<std::list<int> > value)
 {
 	std::cout << "["; 
 	std::for_each(value.pend_.begin(), value.pend_.end(), myPrintInt);
--- ex02/printer.hpp
+++ ex02/printer.hpp
@@ -6,7 +6,7 @@
 /*   By: zpalotas <zpalotas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/16 18:05:11 by zpalotas          #+#    #+#             */
-/*   Updated: 2026/04/16 18:07:34 by zpalotas         ###   ########.fr       */
+/*   Updated: 2026/04/28 14:23:45 by zpalotas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@
 
 void	myPrint(int value);
 void	myPrintInt(int value);
-void	myPrintPair(pendMain value);
+void	myPrintPair(pendMain<std::list<int> >value);
 void	myPrintListList(std::list< std::list<int> > listList, std::string id);
 
 #endif
